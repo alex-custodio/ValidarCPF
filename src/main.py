1eb476ebf1d916118802e1e89024b3a45e319fbc
alexcstd <alex.scustod@gmail.com>
2022-08-15 06:17:20 -0300

    simpliflying code

--- src/main.py
+++ src/main.py
@@ -1,9 +1,5 @@
-# Fazer validador de cpf
-#
-
 cpf = str(input("Digite o seu cpf: "))
-somaDigito1 = 0
-somaDigito2 = 0
+somaDigito1, somaDigito2 = 0,0
 for valorEnumerado, valor in enumerate(cpf[0:9], -10):
     somaDigito1 += abs(valorEnumerado) * int(valor)
     somaDigito2 += abs(valorEnumerado -1) * int (valor)
@@ -11,8 +7,4 @@ operacao1 = 11 - (somaDigito1 % 11)
 digito1 = 0 if operacao1 > 9 else operacao1
 digito2 = 11 - (somaDigito2 % 11)
 novoCpf = cpf[0:9] + str(digito1) + str(digito2)
-if (novoCpf == cpf):
-    print("validado!!!")
-else:
-    print("não foi validado")
-
+print('Validado!' if novoCpf == cpf else 'Não validado')
